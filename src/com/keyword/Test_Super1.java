e623479b34853b8f057c265231dfe8436982150e
suriyajith <suriyamurugan35@gmail.com>
2024-09-21 13:24:25 +0530

    commitmessage

---
+++ src/com/keyword/Test_Super1.java
@@ -0,0 +1,13 @@
+package com.keyword;
+
+public class Test_Super1{
+	public void print()
+	{
+		System.out.println("Super Parent Class");
+	}
+	
+	//public static void main(String[] args) {
+		
+	//}
+
+}
